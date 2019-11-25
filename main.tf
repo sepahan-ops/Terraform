24983b7ef8761dc6f81cae16a5e762a321fc5665
amirakha1 <amir@hashicorp.com>
2019-11-25 16:57:31 -0600

    Added ASG Updated output

--- main.tf
+++ main.tf
@@ -2,10 +2,10 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "exmaple" {
-  ami = "ami-076e36b130f0652ac"
+resource "aws_launch_configuration" "exmaple" {
+  image_id = "ami-076e36b130f0652ac"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  security_groups = [aws_security_group.instance.id]
 
   tags = {
     Name = "terraform-example"
@@ -17,6 +17,31 @@ resource "aws_instance" "exmaple" {
               nohup busybox httpd f p ${var.server_port} &
               EOF
 
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.exmaple.name
+  vpc_zone_identifier = data.aws_subnet_ids.default.id
+
+  max_size = 2
+  min_size = 10
+
+  tag {
+    key = "Name"
+    value = "terraform-asg-example"
+    propagate_at_launch = true
+  }
 }
 
 resource "aws_security_group" "instance" {
@@ -37,6 +62,6 @@ variable "server_port" {
 }
 
 output "public_ip" {
-  value = aws_instance.exmaple.public_ip
+  value = aws_launch_configuration.exmaple.associate_public_ip_address
   description = "The public IP address of the web server"
 }
