80c5f7cf56b889e8d9602b4133868a00e37b778b
amirakha1 <amir@hashicorp.com>
2019-11-25 15:46:10 -0600

    Added user-data, security group, ingress settings

--- main.tf
+++ main.tf
@@ -5,8 +5,27 @@ provider "aws" {
 resource "aws_instance" "exmaple" {
   ami = "ami-076e36b130f0652ac"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
 
   tags = {
     Name = "terraform-example"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index . html
+              nohup busybox httpd f p 8080 &
+              EOF
+
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = 8080
+    protocol = "tcp"
+    to_port = 8080
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
